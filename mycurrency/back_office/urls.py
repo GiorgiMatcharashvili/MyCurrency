232fd0601301de8fb64734e6930cca53dffdf875
GiorgiMatcharashvili <giorgi.matcharashvili2002@gmail.com>
2024-04-21 15:46:05 +0200

    feat(jira_task_id): create a back office of the api, add converter and graph endpoints.

---
+++ mycurrency/back_office/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import Converter, Graph
+
+urlpatterns = [
+    path("converter/", Converter.as_view(), name="back_office_converter"),
+    path("graph/", Graph.as_view(), name="back_office_graph"),
+]
--- mycurrency/mycurrency/settings.py
+++ mycurrency/mycurrency/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "corsheaders",
     # Own
     "mycurrency_api",
+    "back_office",
 ]
 
 MIDDLEWARE = [
